fd38cee94d502e024d18098df917263adfdab9e9
rahul01-git <rahulneupane0090@gmail.com>
2023-07-18 18:12:31 +0545

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const {chats} = require('./data/data')
+dotenv.config()
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("API is running")
+})
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats)
+})
+
+app.get('/api/chat/:id',(req,res)=>{
+    const id = req.params.id
+    const chat = chats.find(chat=>chat._id === id)
+    res.send(chat)
+})
+
+const PORT = process.env.PORT || 6000
+app.listen(PORT,console.log(`Server is running at post ${PORT}`))
