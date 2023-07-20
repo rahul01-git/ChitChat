e58f2f78c13865ccbb27b763cb5c565aeafefd8e
rahul01-git <rahulneupane0090@gmail.com>
2023-07-20 17:08:07 +0545

    auth middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { registerUser, authUser, allUsers } = require('../controllers/user.controller')
+const protect = require('../middlewares/authMiddleware')
 const router = express.Router()
 
-router.route('/').post(registerUser).get(allUsers)
+router.route('/').post(registerUser).get(protect,allUsers)
 router.post('/login',authUser)
 
 router.route('/')
