ed1d05f88f256a9dab0fe15f3d0cc6ac5d25b627
rahul01-git <rahulneupane0090@gmail.com>
2023-07-20 16:53:55 +0545

    search user api

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { registerUser, authUser } = require('../controllers/user.controller')
+const { registerUser, authUser, allUsers } = require('../controllers/user.controller')
 const router = express.Router()
 
-router.post('/', registerUser)
+router.route('/').post(registerUser).get(allUsers)
 router.post('/login',authUser)
+
+router.route('/')
 
 module.exports = router
