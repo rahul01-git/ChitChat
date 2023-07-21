2027a3fa4fefd9d7612300c36889881ed9405a28
rahul01-git <rahulneupane0090@gmail.com>
2023-07-21 13:29:23 +0545

    add and remove from group chat

--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const protect = require('../middlewares/authMiddleware')
-const { accessChat, fetchChats, createGroupChat, renameGroup } = require('../controllers/chat.controller')
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require('../controllers/chat.controller')
 const router = express.Router()
 
 router.route('/').post(protect,accessChat)
 router.route('/').get(protect,fetchChats)
 router.route('/group').post(protect,createGroupChat)
 router.route('/rename').put(protect,renameGroup)
-// router.route('/group-remove').put(protect,removeFromGroup)
-// router.route('/group-add').put(protect,addToGroup)
+router.route('/group-add').put(protect,addToGroup)
+router.route('/group-remove').put(protect,removeFromGroup)
 
 module.exports = router
